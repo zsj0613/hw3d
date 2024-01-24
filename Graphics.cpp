885ad8f7cbfb2f8e062a19a12913d412987ce785
james <zsj06130675@163.com>
2024-01-24 15:56:58 +0800

    Add Pixel Shader

--- Graphics.cpp
+++ Graphics.cpp
@@ -140,6 +140,13 @@ void Graphics::DrawTestTriangle()
 	//Bind Vertex Shader
 	pContext->VSSetShader(pVertexShader.Get(), nullptr, 0u);
 
+	//Create Pixel Shader
+	wrl::ComPtr<ID3D11PixelShader> pPixelShader;
+	GFX_THROW_INFO(D3DReadFileToBlob(L"PixelShader.cso", &pBlob));
+	GFX_THROW_INFO(pDevice->CreatePixelShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pPixelShader));
+	//Bind Vertex Shader
+	pContext->PSSetShader(pPixelShader.Get(), nullptr, 0u);
+
 
 	GFX_THROW_INFO_ONLY(pContext->Draw(3u, 0u));
 }
