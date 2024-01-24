ec3de7678162543ec36d09e48622bb193211ca99
james <zsj06130675@163.com>
2024-01-24 10:35:02 +0800

    Add Exception Handler but Not Work

--- App.cpp
+++ App.cpp
@@ -21,7 +21,7 @@ int App::Go()
 
 void App::DoFrame()
 {
-	const float c = sin(timer.Peek()) / 2.f + 0.5f;
-	wnd.Gfx().ClearBuffer(1.f, c, c);
+	const float c = sin( timer.Peek() ) / 2.0f + 0.5f;
+	wnd.Gfx().ClearBuffer( c,c,1.0f );
 	wnd.Gfx().EndFrame();
 }
--- Graphics.h
+++ Graphics.h
@@ -1,25 +1,52 @@
 #pragma once
 #include "ChiliWin.h"
-#include "d3d11.h"
+#include "ChiliException.h"
+#include <d3d11.h>
+#include <vector>
+#include "DxgiInfoManager.h"
 
-class Graphics 
+class Graphics
 {
 public:
-	Graphics( HWND hWnd);
-	Graphics(const Graphics&) = delete;
-	Graphics& operator=(const Graphics&) = delete;
+	class Exception : public ChiliException
+	{
+		using ChiliException::ChiliException;
+	};
+	class HrException : public Exception
+	{
+	public:
+		HrException( int line,const char* file,HRESULT hr,std::vector<std::string> infoMsgs = {} ) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
+		HRESULT GetErrorCode() const noexcept;
+		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
+		std::string GetErrorInfo() const noexcept;
+	private:
+		HRESULT hr;
+		std::string info;
+	};
+	class DeviceRemovedException : public HrException
+	{
+		using HrException::HrException;
+	public:
+		const char* GetType() const noexcept override;
+	private:
+		std::string reason;
+	};
+public:
+	Graphics( HWND hWnd );
+	Graphics( const Graphics& ) = delete;
+	Graphics& operator=( const Graphics& ) = delete;
 	~Graphics();
 	void EndFrame();
-	void ClearBuffer(float red, float green, float blue) noexcept
-	{
-		const float color[] = {red,green,blue,1.f};
-		pContext->ClearRenderTargetView(pTargetView, color);
-	}
-
+	void ClearBuffer( float red,float green,float blue ) noexcept;
 private:
+#ifndef NDEBUG
+	DxgiInfoManager infoManager;
+#endif
 	ID3D11Device* pDevice = nullptr;
 	IDXGISwapChain* pSwap = nullptr;
 	ID3D11DeviceContext* pContext = nullptr;
-	ID3D11RenderTargetView* pTargetView = nullptr;
-
+	ID3D11RenderTargetView* pTarget = nullptr;
 };
