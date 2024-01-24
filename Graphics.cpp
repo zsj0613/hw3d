b12cd0590b9ae71a9b0c2270840e6054c70fe0cb
james <zsj06130675@163.com>
2024-01-24 16:13:28 +0800

    add primitive topology and input layout , show triangle now

--- Graphics.cpp
+++ Graphics.cpp
@@ -131,24 +131,44 @@ void Graphics::DrawTestTriangle()
 	const UINT stride = sizeof(Vertex);
 	const UINT offset = 0u;
 	pContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
-	
-	//Create vertex Shader
-	wrl::ComPtr<ID3D11VertexShader> pVertexShader;
-	wrl::ComPtr<ID3DBlob> pBlob;
-	GFX_THROW_INFO( D3DReadFileToBlob(L"VertexShader.cso",&pBlob));
-	GFX_THROW_INFO( pDevice->CreateVertexShader(pBlob->GetBufferPointer(),pBlob->GetBufferSize(), nullptr, &pVertexShader));
-	//Bind Vertex Shader
-	pContext->VSSetShader(pVertexShader.Get(), nullptr, 0u);
+
+
 
 	//Create Pixel Shader
 	wrl::ComPtr<ID3D11PixelShader> pPixelShader;
+	wrl::ComPtr<ID3DBlob> pBlob;
 	GFX_THROW_INFO(D3DReadFileToBlob(L"PixelShader.cso", &pBlob));
 	GFX_THROW_INFO(pDevice->CreatePixelShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pPixelShader));
 	//Bind Vertex Shader
 	pContext->PSSetShader(pPixelShader.Get(), nullptr, 0u);
 
+	//Create vertex Shader
+	wrl::ComPtr<ID3D11VertexShader> pVertexShader;
+
+	GFX_THROW_INFO(D3DReadFileToBlob(L"VertexShader.cso", &pBlob));
+	GFX_THROW_INFO(pDevice->CreateVertexShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pVertexShader));
+	//Bind Vertex Shader
+	pContext->VSSetShader(pVertexShader.Get(), nullptr, 0u);
+
+	//Input (vertex ) layout (2d position only)
+	wrl::ComPtr<ID3D11InputLayout> pInputLayout;
+	const D3D11_INPUT_ELEMENT_DESC ied[] =
+	{
+		{"POSITION",0,DXGI_FORMAT_R32G32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0},
+	};
+	GFX_THROW_INFO( pDevice->CreateInputLayout(
+					ied,(UINT)std::size(ied),
+					pBlob->GetBufferPointer(),
+					pBlob->GetBufferSize(),
+					&pInputLayout));
+	//Bind Vertex Layout
+	pContext->IASetInputLayout(pInputLayout.Get());
+
 	//Bind Render Target
 	pContext->OMSetRenderTargets(1u,pTarget.GetAddressOf(),nullptr);
+
+	//Set Primitive topology to triangel list(group of 3 vertices)
+	pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 	
 	//Configure viewport
 	D3D11_VIEWPORT vp;
