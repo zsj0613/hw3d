258fd5e65ad8bc70ac1b25ee8dbe4e1131ffe562
james <zsj06130675@163.com>
2024-01-25 17:38:11 +0800

    solid cube box

--- Shader/VertexShader.hlsl
+++ Shader/VertexShader.hlsl
@@ -1,18 +1,10 @@
-struct VSOut
-{
-    float3 color : Color;
-    float4 pos : SV_POSITION;
-};
 
 cbuffer CBuf
 {
     matrix transform;
 };
 
-VSOut  main( float4 pos : POSITION ,float3 color:Color)
+float4  main( float3 pos : POSITION ):SV_Position
 {
-    VSOut vso;
-    vso.pos = mul(float4(pos.x, pos.y, 0.0f, 1.0f),transform);
-    vso.color = color;
-    return vso;
+    return mul(float4(pos, 1.0f), transform);
 }
