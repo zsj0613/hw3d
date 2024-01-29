7a63b6be123dc5eaf2c2f0bf97f280d580e80bdd
james <zsj06130675@163.com>
2024-01-29 17:59:47 +0800

    Add Bindale/Drawable System

--- App.cpp
+++ App.cpp
@@ -1,9 +1,82 @@
 #include "App.h"
+#include "Melon.h"
+#include "Pyramid.h"
+#include "Box.h"
+#include <memory>
+#include <algorithm>
+#include "ChiliMath.h"
+
 
 App::App()
 	:
 	wnd( 800,600,"The Donkey Fart Box" )
+{
+	class Factory
+	{
+	public:
+		Factory( Graphics& gfx )
+			:
+			gfx( gfx )
+		{}
+		std::unique_ptr<Drawable> operator()()
+		{
+			switch( typedist( rng ) )
+			{
+			case 0:
+				return std::make_unique<Pyramid>(
+					gfx,rng,adist,ddist,
+					odist,rdist
+				);
+			case 1:
+				return std::make_unique<Box>(
+					gfx,rng,adist,ddist,
+					odist,rdist,bdist
+				);
+			case 2:
+				return std::make_unique<Melon>(
+					gfx,rng,adist,ddist,
+					odist,rdist,longdist,latdist
+				);
+			default:
+				assert( false && "bad drawable type in factory" );
+				return {};
+			}
+		}
+	private:
+		Graphics& gfx;
+		std::mt19937 rng{ std::random_device{}() };
+		std::uniform_real_distribution<float> adist{ 0.0f,PI * 2.0f };
+		std::uniform_real_distribution<float> ddist{ 0.0f,PI * 0.5f };
+		std::uniform_real_distribution<float> odist{ 0.0f,PI * 0.08f };
+		std::uniform_real_distribution<float> rdist{ 6.0f,20.0f };
+		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
+		std::uniform_int_distribution<int> latdist{ 5,20 };
+		std::uniform_int_distribution<int> longdist{ 10,40 };
+		std::uniform_int_distribution<int> typedist{ 0,2 };
+	};
+
+	Factory f( wnd.Gfx() );
+	drawables.reserve( nDrawables );
+	std::generate_n( std::back_inserter( drawables ),nDrawables,f );
+
+	wnd.Gfx().SetProjection( DirectX::XMMatrixPerspectiveLH( 1.0f,3.0f / 4.0f,0.5f,40.0f ) );
+}
+
+void App::DoFrame()
+{
+	const auto dt = timer.Mark();
+	wnd.Gfx().ClearBuffer( 0.07f,0.0f,0.12f );
+	for( auto& d : drawables )
+	{
+		d->Update( dt );
+		d->Draw( wnd.Gfx() );
+	}
+	wnd.Gfx().EndFrame();
+}
+
+App::~App()
 {}
+
 
 int App::Go()
 {
@@ -17,21 +90,4 @@ int App::Go()
 		}
 		DoFrame();
 	}
-}
-
-void App::DoFrame()
-{
-	const float c = sin( timer.Peek() ) / 2.0f + 0.5f;
-	wnd.Gfx().ClearBuffer( c,c,1.0f );
-
-	wnd.Gfx().DrawTestTriangle(
-		-timer.Peek(),
-		0.f,
-		0.f);
-
-
-	wnd.Gfx().DrawTestTriangle(timer.Peek(),
-								wnd.mouse.GetPosX()/400.f -1.f, 
-								1.f - wnd.mouse.GetPosY()/300.f);
-	wnd.Gfx().EndFrame();
 }
--- App.h
+++ App.h
@@ -8,9 +8,12 @@ public:
 	App();
 	// master frame / message loop
 	int Go();
+	~App();
 private:
 	void DoFrame();
 private:
 	Window wnd;
 	ChiliTimer timer;
+	std::vector<std::unique_ptr<class Drawable>> drawables;
+	static constexpr size_t nDrawables = 180;
 };
