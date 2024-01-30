66bffb0604f95e119e60a55096d23a3805d8934b
james <zsj06130675@163.com>
2024-01-30 11:05:06 +0800

    Texture/Sampler/Sheet Drawable

--- App.cpp
+++ App.cpp
@@ -7,6 +7,7 @@
 #include "ChiliMath.h"
 #include "Surface.h"
 #include "GDIPlusManager.h"
+#include "Sheet.h"
 
 GDIPlusManager gdipm;
 
@@ -40,6 +41,11 @@ App::App()
 					gfx,rng,adist,ddist,
 					odist,rdist,longdist,latdist
 				);
+			case 3:
+				return std::make_unique<Sheet>(
+					gfx, rng, adist, ddist,
+					odist, rdist
+				);
 			default:
 				assert( false && "bad drawable type in factory" );
 				return {};
@@ -55,14 +61,13 @@ App::App()
 		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
 		std::uniform_int_distribution<int> latdist{ 5,20 };
 		std::uniform_int_distribution<int> longdist{ 10,40 };
-		std::uniform_int_distribution<int> typedist{ 0,2 };
+		std::uniform_int_distribution<int> typedist{ 0,3 };
 	};
 
 	Factory f( wnd.Gfx() );
 	drawables.reserve( nDrawables );
 	std::generate_n( std::back_inserter( drawables ),nDrawables,f );
 
-	const auto s = Surface::FromFile( "Images\\kappa50.png" );
 
 	wnd.Gfx().SetProjection( DirectX::XMMatrixPerspectiveLH( 1.0f,3.0f / 4.0f,0.5f,40.0f ) );
 }
@@ -73,7 +78,7 @@ void App::DoFrame()
 	wnd.Gfx().ClearBuffer( 0.07f,0.0f,0.12f );
 	for( auto& d : drawables )
 	{
-		d->Update( dt );
+		d->Update(wnd.kbd.KeyIsPressed(VK_SPACE) ? 0.0f : dt);
 		d->Draw( wnd.Gfx() );
 	}
 	wnd.Gfx().EndFrame();
